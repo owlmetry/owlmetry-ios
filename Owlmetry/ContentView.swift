78411af4d3d696b2a71b90932c5c6ffdfe9244d3
Jason van den Berg <jayvdb1@gmail.com>
2026-04-24 14:35:43 +0200

    scaffold blank Xcode iOS project

---
+++ Owlmetry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Owlmetry
+//
+//  Created by Jason on 2026/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Owlmetry/OwlmetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlmetryApp.swift
+//  Owlmetry
+//
+//  Created by Jason on 2026/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OwlmetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
